18af6d076a9d55846d601e7fb1b81716321510d9
hadri12velay <hadri1.2velay@gmail.com>
2022-03-28 13:18:41 +0100

    update calculator proj

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,29 @@
+$.fn.myFunction = function(x){
+    console.log(x.hasClass('details'));
+}
+
 $('.details').click(function() {
+    let ID = $(this).attr('id');
+
     if($(this).hasClass('active')) {
         if($(this).hasClass('showClose')) {
             $(this).html('details;');
         }
         $(this).toggleClass('active');
-        $('#' + $(this).attr('id') + 'details').toggleClass('hidden')
-        console.log($(this).attr('id') + 'details')
-        console.log('closing details...')
+        $('#' + ID + 'details').toggleClass('hidden');
+        console.log(ID + 'details');
+        console.log('closing details...');
+        $.fn.myFunction($(this));
         
     } else {
         if($(this).hasClass('showClose')) {
             $(this).html('close;');
         }
         $(this).toggleClass('active');
-        $('#' + $(this).attr('id') + 'details').toggleClass('hidden')
-        console.log($(this).attr('id') + 'details')
-        console.log('opening details...')
-
-
+        $('#' + ID + 'details').toggleClass('hidden');
+        console.log(ID + 'details');
+        console.log('opening details...');
+        $.fn.myFunction($(this));
     }
     
 });
